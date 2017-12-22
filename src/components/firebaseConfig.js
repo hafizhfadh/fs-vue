735f2fc5ab192a163cc6616992b738969c46dbed
Hafizh Fadhlurrohman <hafizhfadh94@gmail.com>
2017-12-22 16:18:03 +0700

    Final Commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyCwK7BCdUnuMLOkAS23w7CCC464yXe7ZHU",
+    authDomain: "vuefs-prod-b8f96.firebaseapp.com",
+    databaseURL: "https://vuefs-prod-b8f96.firebaseio.com",
+    projectId: "vuefs-prod-b8f96",
+    storageBucket: "vuefs-prod-b8f96.appspot.com",
+    messagingSenderId: "594371284391"
+}
